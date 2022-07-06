b15d36665c20e1db2697ff6bce6f79444f3aaa94
kimdoyoung <57849386+coby5502@users.noreply.github.com>
2022-07-06 22:54:45 +0900

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by COBY_PRO on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
