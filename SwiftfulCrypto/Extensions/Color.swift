be72ed657bd93badf93cd2f012197499c63eb02f
Doyoung <coby5502@gmail.com>
2022-07-06 23:04:23 +0900

    SwiftUI Crypto App #1
    Setting up Xcode Project and a custom color theme

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by COBY_PRO on 2022/07/06.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
