74cbd3c1f36746ffb8a900fe41bd53e1ace85aa6
Doyoung <coby5502@gmail.com>
2022-07-07 21:13:36 +0900

    SwiftUI Crypto App #4
    Design a row to display coins in a List

--- SwiftfulCrypto/Core/Components/CircleButton/CircleButtonView.swift
+++ SwiftfulCrypto/Core/Components/CircleButton/CircleButtonView.swift
@@ -38,7 +38,7 @@ struct CircleButtonView_Previews: PreviewProvider {
             
             CircleButtonView(iconName: "plus")
                 .previewLayout(.sizeThatFits)
-                .colorScheme(.dark)
+                .preferredColorScheme(.dark)
         }
     }
 }
